6aae0744581f3dbbdf7e95ae9075843257bbdc7a
Youngjae Choi <cdw0807@naver.com>
2022-08-11 17:00:56 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,13 +16,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
